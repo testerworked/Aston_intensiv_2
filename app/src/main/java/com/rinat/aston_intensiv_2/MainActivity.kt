9a56aaed13b9a54d277e2bcd5731828f4441f40c
Khuzin Rinat <r.khuzin@wrtech.ru>
2023-05-13 11:05:52 +0500

    add country flags

---
+++ app/src/main/java/com/rinat/aston_intensiv_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rinat.aston_intensiv_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
