438b086cec6826b95e428b4020af7d36de20b352
Khuzin Rinat <gipnoz.rinat@gmail.com>
2023-05-16 21:16:18 +0500

    load flags from EditText

--- app/src/main/java/com/rinat/aston_intensiv_2/SecondTask.kt
+++ app/src/main/java/com/rinat/aston_intensiv_2/SecondTask.kt
@@ -2,19 +2,57 @@ package com.rinat.aston_intensiv_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.widget.doOnTextChanged
+import coil.imageLoader
+import coil.request.ImageRequest
+import kotlinx.coroutines.*
 
 class SecondTask : AppCompatActivity() {
+
+    private val imageLoadFlags by lazy {
+        findViewById<ImageView>(R.id.imageLoadFlags)
+    }
+
+    private val searchWords by lazy {
+        findViewById<EditText>(R.id.editSearchWords)
+    }
+    var urlForFlagDownload: String = ""
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_task)
 
-//        val imageLoader = imageView.context.imageLoader
-//        val request = ImageRequest.Builder(imageView.context)
-//            .data("https://example.com/image.jpg")
-//            .target(imageView)
-//            .build()
-//        imageLoader.enqueue(request)
+
+
+        GlobalScope.launch(Dispatchers.IO) {
+            searchWords.doOnTextChanged { textFromSearch, _, _, _ ->
+                //updateTextView(text.toString())
+                GlobalScope.launch(Dispatchers.Main) {
+                    urlForFlagDownload = textFromSearch.toString()
+                }
+            }
+        }
+
+        GlobalScope.launch{
+            delay(1000L)
+
+            val imageLoader = imageLoadFlags.context.imageLoader
+            val request = ImageRequest.Builder(imageLoadFlags.context)
+                //.data("https://sportishka.com/uploads/posts/2022-03/1647938865_67-sportishka-com-p-krasota-zimnego-baikala-turizm-krasivo-fot-71.jpg")
+                .data(urlForFlagDownload)
+                .target(imageLoadFlags)
+                .build()
+            imageLoader.enqueue(request)
+        }
+
+
+
     }
+
 
 }
